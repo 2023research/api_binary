78be4bb08331c66ac7d8571990c6e3b516ecaf33
13706635 <shuming.liang@student.uts.edu.au>
2023-12-16 12:09:24 +1100

    win16dec

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from model2000API import xgb_predictor
 app = FastAPI()
 class_dict = {0:'non-maintenance',1:'maintenance'}
 
+# what is an emial related to
 @app.post("/classfier")
 def read_item(email: str):
     # print (id)
