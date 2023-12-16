7a304c8b2bc31076818cc6fe72d18198f03eba74
13706635 <shuming.liang@student.uts.edu.au>
2023-12-16 11:11:28 +1100

    v2

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 from model2000API import xgb_predictor
 
 app = FastAPI()
+class_dict = {0:'non-maintenance',1:'maintenance'}
 
-@app.get("/binaryclass/{email}")
-async def binaryclassifier(email: str):
-    return {'prediction':[email]}
 
-@app.post("/binaryclassfier")
+@app.post("/classfier")
 def read_item(email: str):
     # print (id)
-    return {'prediction': xgb_predictor([email]).item()}
+    return {'prediction': class_dict[xgb_predictor([email]).item()]}
     # return {"item_id": item_id}
