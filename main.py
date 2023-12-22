74c706bf2fa5bee4eab7ca6286faad9522032d9d
13706635 <shuming.liang@student.uts.edu.au>
2023-12-22 16:46:03 +1100

    dec2221

--- main.py
+++ main.py
@@ -15,13 +15,25 @@ from starlette.middleware.cors import CORSMiddleware
 
 # app = FastAPI(middleware=middleware)
 app = FastAPI()
+
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "https://www.teste.com.br, http://0.0.0.0:8000"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     # allow_credentials=True,
-    # allow_methods=["*"],
-    # allow_headers=["*"],
-    # expose_headers=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
