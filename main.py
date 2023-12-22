bbc41af128938f6226d9caf04db9347bbe873d0f
13706635 <shuming.liang@student.uts.edu.au>
2023-12-22 15:51:34 +1100

    dec2215

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=['*'],
-        allow_credentials=True,
+        # allow_credentials=True,
         allow_methods=['*'],
         allow_headers=['*']
     )
